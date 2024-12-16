9b69de2f0de90b60dc3ee305d272136d5160c2c0
OrakomoRi <OrakomoRi@gmail.com>
2024-12-16 04:45:48 +0200

    1.6.0-alpha5 feat: new image load method (not base64)

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -248,42 +248,35 @@
 	const imageLinks = [
 		{
 			url: 'https://github.com/OrakomoRi/Severitium/blob/main/src/.images/png/General/CommonContainer.png?raw=true',
-			style: '.Common-container:not(.Common-entranceBackground){background-image:url(data:image/png;base64,SEVERITIUM_PLACEHOLDER)}'
+			style: '.Common-container:not(.Common-entranceBackground){background-image:url(SEVERITIUM_PLACEHOLDER)}'
 		},
 		{
 			url: 'https://github.com/OrakomoRi/Severitium/blob/main/src/.images/png/Lobby/PlayButton.png?raw=true',
-			style: '.MainScreenComponentStyle-playButtonContainer{background-image:url(data:image/png;base64,SEVERITIUM_PLACEHOLDER)}'
+			style: '.MainScreenComponentStyle-playButtonContainer{background-image:url(SEVERITIUM_PLACEHOLDER)}'
 		},
 		{
 			url: `https://github.com/OrakomoRi/Severitium/blob/main/src/.images/png/Entrance/${_getSeason()}.png?raw=true`,
-			style: '.Common-container.Common-entranceBackground, .Common-background.SystemMessageStyle-container, .Common-container:has(.Common-changingBackground){background-image:url(data:image/png;base64,SEVERITIUM_PLACEHOLDER)}'
+			style: '.Common-container.Common-entranceBackground, .Common-background.SystemMessageStyle-container, .Common-container:has(.Common-changingBackground){background-image:url(SEVERITIUM_PLACEHOLDER)}'
 		},
 	];
 
-	function applyStyles(base64data, styleTemplate) {
-		const styledBackground = styleTemplate.replace('SEVERITIUM_PLACEHOLDER', base64data);
+	function applyStyles(imageUrl, styleTemplate) {
+		const styledBackground = styleTemplate.replace('SEVERITIUM_PLACEHOLDER', imageUrl);
 		
-		var styleElement = document.createElement('style');
+		const styleElement = document.createElement('style');
 		styleElement.textContent = styledBackground;
 		document.body.appendChild(styleElement);
 	}
 
-	function fetchImageAsBase64(url, styleTemplate) {
+	function fetchImageUrl(url, styleTemplate) {
 		GM_xmlhttpRequest({
 			method: 'GET',
 			url: url,
 			responseType: 'blob',
 			onload: function(response) {
 				if (response.status === 200) {
-					const blob = response.response;
-					const reader = new FileReader();
-					
-					reader.onloadend = function() {
-						const base64data = reader.result.split(',')[1];
-						applyStyles(base64data, styleTemplate);
-					};
-	
-					reader.readAsDataURL(blob);
+					const imageUrl = URL.createObjectURL(response.response);
+					applyStyles(imageUrl, styleTemplate);
 				} else {
 					console.error('SEVERITIUM: Failed to load image from:', url);
 				}
@@ -295,5 +288,5 @@
 	}
 
 	// Iterate over each image link and apply the corresponding style
-	imageLinks.forEach(({ url, style }) => fetchImageAsBase64(url, style));
+	imageLinks.forEach(({ url, style }) => fetchImageUrl(url, style));
 })();
