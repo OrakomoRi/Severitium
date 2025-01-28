36ea2ddb5e9139529123792f202518ffd11d15a0
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-01-28 19:36:10 +0200

    1.6.1+build52 MAIN USERSCRIPT

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.6.1+build51
+// @version			1.6.1+build52
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
