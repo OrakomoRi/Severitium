a1df0cc4f8c2069da9a369d2ba2be15bb3a4be47
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-02-01 01:17:19 +0200

    1.6.1+build68 fix: fixed link in json to Shop/MainScreen CSS

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.6.1+build67
+// @version			1.6.1+build68
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
