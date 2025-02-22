a3c57e673e0ca5a9f59d00f3530806f92ace2f13
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-02-22 16:36:53 +0200

    1.6.1+build98 fix: fixed superquest button container hight

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.6.1+build97
+// @version			1.6.1+build98
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
