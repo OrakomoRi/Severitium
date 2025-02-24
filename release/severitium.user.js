4d8c4e8a50ab64b34b6f9e58880ffcbaafc37afa
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-02-24 12:52:19 +0200

    1.6.1+build103 feat: updated main theme color

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.6.1+build102
+// @version			1.6.1+build103
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
--- src/Variables/Variables.user.js
+++ src/Variables/Variables.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			CSS Variables
-// @version			1.4.5
+// @version			1.4.56
 // @description		Injects CSS variables into the page
 // @author			OrakomoRi
 
