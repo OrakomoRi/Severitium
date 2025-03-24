279cf65dfc73294bdff7cdf0e4ae53a342a72ce9
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-03-25 00:21:18 +0200

    1.7.1+build16 feat: updated payment screen

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.1+build15
+// @version			1.7.1+build16
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
