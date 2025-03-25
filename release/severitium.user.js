69448a04bf6d65a81dbe0b6e1d67e1fbffd6dfbc
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-03-25 15:49:15 +0200

    1.7.1+build19 feat: updated entrance forms (2)

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.1+build18
+// @version			1.7.1+build19
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
--- src/Entrance/EntranceForms/EntranceForms.user.js
+++ src/Entrance/EntranceForms/EntranceForms.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			CSS Entrance Forms
-// @version			1.4.3
+// @version			1.4.4
 // @description		Injects CSS code into the page
 // @author			OrakomoRi
 
