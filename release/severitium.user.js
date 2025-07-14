2c516f6c65cb62a3143b92be2868641d6e4b9cca
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-07-14 03:50:44 +0300

    1.7.2+build17 chore: remove release/data — moved to 'builds' branch

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build16
+// @version			1.7.2+build17
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
