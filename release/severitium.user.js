d257c587db6f41a031675224ba589a1202eeca18
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-07-14 03:35:49 +0300

    1.7.2+build16 feat: moved auto-release and build-theme into one yml: auto-build-and-release

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build15
+// @version			1.7.2+build16
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
