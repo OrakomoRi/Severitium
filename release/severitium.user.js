78f0c7b8e239da96a5deafef33bc596ef16cc0b2
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-07-14 03:30:36 +0300

    1.7.2+build15 feat: created auto-release feature

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build14
+// @version			1.7.2+build15
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
