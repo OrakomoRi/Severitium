7925d8e30ef051566fdf13bf1922e9cb82814e21
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-25 04:11:42 +0300

    1.7.2+build27 chore(release): trigger new release

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build27
+// @version			1.7.2+build28
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
