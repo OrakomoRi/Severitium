42d881a454d00cea7d52a7a0e81f13cbe5e87964
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-25 04:02:01 +0300

    1.7.2+build27 feat: add transition effects to shop window styles

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build26
+// @version			1.7.2+build27
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
