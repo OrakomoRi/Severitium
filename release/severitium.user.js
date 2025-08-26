33f61c729cabc53aceb70aee956ece1ba1f73051
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-27 00:04:37 +0300

    1.7.2+build33 feat: updated all css selectors to use ' instead of " fix: fixed existing clan window (actions container), fixed entrance input selectors

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build32
+// @version			1.7.2+build33
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
