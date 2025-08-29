66e0ad4b778d9651847a8d6af33180e229707bb9
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-30 01:45:56 +0300

    1.7.2+build41 feat: updated button styles for modals

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build40
+// @version			1.7.2+build41
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
