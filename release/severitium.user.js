043af40130f33e212dcdbe84efec49acd7bf6ca4
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-30 01:09:36 +0300

    1.7.2+build36 feat: update css minifier (cleancss -> lightningcss)

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build35
+// @version			1.7.2+build36
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
