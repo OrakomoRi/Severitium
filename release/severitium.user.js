55264b98a18cf1220499917bff56d250a16f27e0
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-30 01:12:04 +0300

    1.7.2+build37 fix: npm now downloads correct minifier

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build36
+// @version			1.7.2+build37
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
