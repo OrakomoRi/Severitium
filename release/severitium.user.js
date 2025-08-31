2f7e9021dd5efc4c11ad1725e418ecf4d872a969
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-31 23:20:29 +0300

    1.7.2+build59 feat: updated entrance forms and notificator icon

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build58
+// @version			1.7.2+build59
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
