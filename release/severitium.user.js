e3fed6ad21b6d37305405413b5e257c365602d7c
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-01 01:15:06 +0300

    1.7.2+build62 feat: updated notificator icon

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build61
+// @version			1.7.2+build62
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
