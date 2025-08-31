66777e4aba26e1f31905c282403a7bb8b08b8fa0
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-08-31 03:52:10 +0300

    1.7.2+build58 feat: chat window update

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build57
+// @version			1.7.2+build58
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
