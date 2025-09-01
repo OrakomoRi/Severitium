b15a7f0348f9a907247e93689536a025ac27723f
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-01 17:47:35 +0300

    1.7.2+build63 feat: updated entrance forms

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build62
+// @version			1.7.2+build63
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
