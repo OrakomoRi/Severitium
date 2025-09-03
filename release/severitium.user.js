786cc0565afdb135bdb03e562804ed8cc9b786aa
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-03 23:05:16 +0300

    1.7.2+build86 feat: updated friends and invite screens

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build85
+// @version			1.7.2+build86
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
