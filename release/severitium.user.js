86a39d7da0692101f7c591d63a454cf89ce381a1
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-04 15:39:50 +0300

    1.7.2+build90 feat: updated shop window styles

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build89
+// @version			1.7.2+build90
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
