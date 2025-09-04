173a8b9e5f04977ed49bcaf4bfcb4710ed9c07ed
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-04 12:59:47 +0300

    1.7.2+build89 revert: revert of previously commited 'not ready' files

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build88
+// @version			1.7.2+build89
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
