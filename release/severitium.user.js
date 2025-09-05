8b966e7947e62658f54daec5bf6a552afbd83332
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-05 04:40:38 +0300

    1.7.2+build125 feat: updated breezium select, theme settings

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build124
+// @version			1.7.2+build125
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
