6fdc88311384aaa8d02e336108cdecbcc6625625
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-06 17:16:39 +0300

    1.7.2+build136 feat: updated settings content

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build135
+// @version			1.7.2+build136
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
