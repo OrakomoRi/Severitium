380196ae63296c7df1e5597db4a84a6543ff02df
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-12 17:14:27 +0300

    1.7.2+build144 feat: updated footer menu

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build143
+// @version			1.7.2+build144
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
