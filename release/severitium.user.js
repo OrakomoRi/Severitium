8b96d34fa20bb1f4de71523919ac3b243b4a5ef0
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-14 14:03:31 +0300

    1.7.2+build148 fix: file collection logic in auto-build workflow

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.7.2+build147
+// @version			1.7.2+build148
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
