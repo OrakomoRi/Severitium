7b8b8d88d5a36186c3f4970a1a781c112f3f8b9f
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-25 21:21:15 +0300

    1.8.0+build.4 feat: updated theme colors and styles for selection in various components

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.0+build.3
+// @version			1.8.0+build.4
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
