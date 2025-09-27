09138c90ace738399420d80c7ccdf1d1a6bfa0b7
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-27 17:23:49 +0300

    1.8.0+build.6 fix: fixed loading screen display

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.0+build.5
+// @version			1.8.0+build.6
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
