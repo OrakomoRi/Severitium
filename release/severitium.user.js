e1abb1ea71f9aafcfde98fcdb77466ebe4ab1018
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-09-30 01:27:41 +0300

    1.8.0+build.16 feat: loading screen overlay should be removed

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.0+build.15
+// @version			1.8.0+build.16
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
