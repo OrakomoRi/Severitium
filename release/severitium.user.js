0cd08e97a47d601179aaaa8a6083a82226279479
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-10-02 02:00:09 +0300

    1.8.0+build.19 feat: updated styles for challenges

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.0+build.18
+// @version			1.8.0+build.19
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
