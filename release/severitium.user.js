a6fb724a51a536b783ddb7406fe42a3495e7df5e
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-10-15 04:40:52 +0300

    1.8.1+build10 feat: updated modal

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.1+build9
+// @version			1.8.1+build10
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
