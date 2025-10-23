b0de6302ff76c78f7a7c5d68d0bac692cf63bd73
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-10-23 04:11:03 +0300

    1.8.1+build14
    chore: update version number and enhance auto-build workflow
    
    - Update version number in severitium.user.js to 1.8.1+build14
    - Refactor version stability check to use SemVer regex
    - Improve artifact building process with temporary directory
    - Add vercel.json for deployment configuration
    - Enhance commit info preparation and version tracking

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.1+build13
+// @version			1.8.1+build14
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
