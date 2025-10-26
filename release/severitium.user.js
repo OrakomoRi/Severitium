28a5cb6a5bffa2c4f9ed84aefb1a85b605e44dcb
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-10-26 15:30:00 +0200

    1.8.2+build.9
    chore: update version and add ClientInfo CSS
    
    - Update version number to 1.8.2+build.9
    - Add ClientInfoComponentStyle-main class to hide the component

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.8
+// @version			1.8.2+build.9
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
