582ad7cb9e1f3a927b70743da1b1f1cf5af81131
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-03 22:08:00 +0200

    1.8.2+build.21
    chore: update version and improve hover styles
    
    - Increment version to 1.8.2+build.21
    - Modify hover styles for primary menu items
    - Refine hover behavior for main menu buttons

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.20
+// @version			1.8.2+build.21
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
