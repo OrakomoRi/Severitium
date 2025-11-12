0064ab67ff83c3607e2d5779ab0140fd91a8d8c1
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-13 00:06:56 +0200

    1.8.2+build.27
    feat: update version and add SVG icon
    
    - Update version number to 1.8.2+build.27
    - Add new SVG icon for the user info container
    - Modify CSS to display the custom icon instead of the default one

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.26
+// @version			1.8.2+build.27
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
