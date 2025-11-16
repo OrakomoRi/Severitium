df539888733bda99b15ad5163d04093d1ebc8693
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-16 14:07:43 +0200

    1.8.2+build.28
    feat: relocate custom hover glitch effect and icon styling
    
    - Added a new CSS file for a custom icon in the user info container.
    - Removed the previous icon styling from `TopPanel.css` and centralized it in the new `CollabLogo.css`.
    - Updated the version number to reflect the new features and changes.

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.27
+// @version			1.8.2+build.28
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
