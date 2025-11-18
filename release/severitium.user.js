2aa67b9aa83255456eaea64a0f656fa8d27d5a61
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-18 11:10:05 +0200

    1.8.2+build.36
    chore: update version and improve CSS styles
    
    - Update version number to 1.8.2+build.36
    - Improved quests screen styles when pressing on quests card

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.35
+// @version			1.8.2+build.36
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
