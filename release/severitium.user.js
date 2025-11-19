7e3f3293ac5fea146b3e8ff9781f9258f681c3a3
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-19 06:52:37 +0200

    1.8.2+build.39
    chore: update version number and change collab icon
    
    - Increment version number to 1.8.2+build.39
    - Change collab icon to make 'o' letter as game icon

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.38
+// @version			1.8.2+build.39
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
