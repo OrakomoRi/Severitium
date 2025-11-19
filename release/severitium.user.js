bb3b9f11f6e712d152a0935c7b51f91a4b16e8cf
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-19 06:55:12 +0200

    1.8.2+build.40
    chore: update version and improve CSS comments
    
    - Updated version number in severitium.user.js to 1.8.2+build.40
    - Changed comment in CollabLogo.css to clarify purpose
    - Updated mask-image URL in CollabLogo.css for improved SVG rendering

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.39
+// @version			1.8.2+build.40
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
