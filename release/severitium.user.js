ca303983e385ed17f6dc1838058879623c15bc0c
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-19 06:34:52 +0200

    1.8.2+build.38
    feat: enhance quest description modal
    
    - Update modal styles for quest description timer block
    - Add padding, background color, and border radius to timer block
    - Style timer text with increased font size and letter spacing

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.37
+// @version			1.8.2+build.38
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
