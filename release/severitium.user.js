943812ea21b6091e9d43548b99d47310a9318210
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-23 17:19:15 +0200

    1.8.2+build.56
    chore: update version and add new SVG asset
    
    - Update version number to 1.8.2+build.56
    - Add new SVG file for sunburst graphic
    - Refactor CSS to improve filter properties and add fit-content adjustments
    - Enhance modal animations and styles for rankup modal
    - Update button hover states and improve layout responsiveness

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.55
+// @version			1.8.2+build.56
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
