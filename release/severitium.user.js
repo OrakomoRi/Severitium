98a22c83b0c4347accb7ca28d6044a59594f471e
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-23 10:33:11 +0200

    1.8.2+build.54
    feat: enhance quest card styles
    
    - Add pressed state styles for completed solo quest cards
    - Implement pressed state styles for quest solo lines
    - Update styles for quest completed solo line cards

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.53
+// @version			1.8.2+build.54
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
