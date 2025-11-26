5130a1074c5f536ee5d829a2bbcf9d91a33992e9
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-11-26 15:53:53 +0200

    1.8.2+build.58
    feat: update version and enhance premium modal styles
    
    - Update version number to 1.8.2+build.58
    - Add few more styles for premium modal

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.57
+// @version			1.8.2+build.58
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
