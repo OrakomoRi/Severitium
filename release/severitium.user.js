13773686a989b3b74f2d4ba1896fb8df69517e98
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-12-03 19:34:38 +0200

    1.8.2+build.68
    chore: update version and refine modal styles
    
    - Increment version number to 1.8.2+build.68
    - Refine modal root container styles to exclude specific class elements

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.67
+// @version			1.8.2+build.68
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
