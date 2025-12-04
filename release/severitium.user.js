03e37926beff79ea911ec192a710549b957aab5c
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-12-04 18:59:01 +0200

    1.8.2+build.72
    feat: update version and enhance modal styles
    
    - Update version number in severitium.user.js
    - Add new styles for close button container in Modal.css
    - Implement styles for gearscore modal, including layout and button designs

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.2+build.71
+// @version			1.8.2+build.72
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
