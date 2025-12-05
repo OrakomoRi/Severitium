835df5891d25087322d36c6dae13455bd22b8034
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-12-05 23:42:51 +0200

    1.8.3+build.3
    chore: update version and refine modal styles
    
    - Increment version to 1.8.3+build.3
    - Refine modal styles to exclude reCAPTCHA block from hover effects

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.3+build.2
+// @version			1.8.3+build.3
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
