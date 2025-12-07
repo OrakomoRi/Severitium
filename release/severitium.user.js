2415f5411cfae9ca780910dbd80e8b2c1c2ef8ee
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-12-07 13:19:57 +0200

    1.8.3+build.11
    feat: update styles and add new variables
    
    - Increment version to 1.8.3+build.11
    - Add opacity to challenge button styles
    - Introduce new styles for elite challenges and progress bars
    - Add gradient overlays for main menu
    - Define new timer color variable

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.3+build.10
+// @version			1.8.3+build.11
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
