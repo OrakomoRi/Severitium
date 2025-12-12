6b4954c4eb85b5072c007f9c5f15dcf1f9923d6b
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2025-12-12 13:29:19 +0200

    1.8.3+build.17
    chore: update version and modify text color
    
    - Increment version to 1.8.3+build.17
    - Change text color from white to black in ChallengesScreen.css

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.3+build.16
+// @version			1.8.3+build.17
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
