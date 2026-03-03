b73312a0ecb29f93381817af42be9afd359b7bb0
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-03-03 19:45:19 +0200

    1.8.3+build.24
    feat: enhance rank progress styles
    
    - Add gradient transition effects for completed ranks
    - Implement hover and active states for completed and uncompleted ranks
    - Update CSS for improved visual feedback on rank progress

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.3+build.23
+// @version			1.8.3+build.24
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
