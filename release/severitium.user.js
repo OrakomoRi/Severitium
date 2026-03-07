238aeef46e72befb0e3286169ec5328fd61ed3a1
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-03-08 01:58:33 +0200

    1.8.3+build.49
    feat: update styles for entrance and loading screens
    
    - Refactor CSS selectors in EntranceBackground.css for consistency
    - Update styles in ImageModules.json to match new class naming conventions
    - Add loading screen overlay styles in LoadingScreen.css

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.8.3+build.47
+// @version			1.8.3+build.49
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
