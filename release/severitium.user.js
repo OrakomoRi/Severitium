b24bd5ca5eaed10ce760ba3386b502bb4eb635bb
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-04-04 22:01:14 +0300

    1.9.3+build17
    style: update button transition effects
    
    - Enhance transition effects for .MountedItemsStyle-commonForCellResistenceName
    - Adjust justify-content property in .QuestsComponentStyle-content
    - Set width to 100% for quest items in .MainQuestComponentStyle-scrollContainer

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build16
+// @version			1.9.3+build17
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
