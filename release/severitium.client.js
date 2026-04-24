29c07a976f50b84dc3d5ba628c58847919680246
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-04-24 12:45:04 +0300

    1.9.3+build49
    fix: update modal header styling
    
    - Adjusted CSS selector for ItemDescriptionComponentStyle to target header elements

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build48';
+	const CLIENT_VERSION = '1.9.3+build49';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build48
+// @version			1.9.3+build49
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
