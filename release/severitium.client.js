5567215b5de33f24da517acd5007075b11c87dba
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-05-10 21:30:44 +0300

    1.9.3+build65
    style: update card styles
    
    - Change height to aspect-ratio for card images in BattleType.css
    - Update mask-image property to use CSS variable in BattleType.css

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build64';
+	const CLIENT_VERSION = '1.9.3+build65';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build64
+// @version			1.9.3+build65
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
