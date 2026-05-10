d9d48e2be9c23dac4931b3f3d6e4278f47d00f3e
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-05-11 00:02:59 +0300

    1.9.3+build67
    style: update notificator icon styles
    
    - Refactor mask-image properties in NotificatorIcon.css to use CSS variables
    - Improve maintainability by removing inline SVG data

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build66';
+	const CLIENT_VERSION = '1.9.3+build67';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build66
+// @version			1.9.3+build67
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
