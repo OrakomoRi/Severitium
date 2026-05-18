993358abb885fc745937fb45169fc5736cf2469e
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-05-18 17:18:57 +0300

    1.9.3+build105
    style: update layout for reward description block
    
    - Added flexbox layout to .ContainerInfoComponentStyle-learnMoreBlock
    - Changed cursor style to default for certain elements
    - Adjusted margin for .ContainerInfoComponentStyle-rewardDescriptionBlock

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build104';
+	const CLIENT_VERSION = '1.9.3+build105';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build104
+// @version			1.9.3+build105
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
