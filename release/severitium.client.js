6a1b7d06c7f12a536ab7b659d8610294bec8993b
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-05-19 01:18:26 +0300

    1.9.3+build113
    refactor: improve card rarity processing
    
    - Replace singleClearAllInlineStyle with continuousClearAllInlineStyle in ContractsScreen.js
    - Simplify card rarity processing logic in onMutation handler
    - Update rarity attribute setting for ContractCardComponentStyle-card elements

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build112';
+	const CLIENT_VERSION = '1.9.3+build113';
 
 	const isLogging = false;
 
--- src/modules/Quests/ContractsScreen/ContractsScreen.js
+++ src/modules/Quests/ContractsScreen/ContractsScreen.js
@@ -1,30 +1,28 @@
 import { onMutation } from '../../../libs/modules/MutationHandler/MutationHandler.js';
-import { singleClearAllInlineStyle } from '../../../libs/modules/InlineStyleRemover/InlineStyleRemover.js';
+import { continuousClearAllInlineStyle } from '../../../libs/modules/InlineStyleRemover/InlineStyleRemover.js';
 import { RARITY_COLORS } from '../../../libs/modules/constants/RarityColors.js';
 
 (function () {
 	const CARD = '.ContractCardComponentStyle-card';
 
-	function processCard(card) {
-		const rarityBlock = card.querySelector('div.-backgroundImageContain + div');
-		if (rarityBlock) {
-			rarityBlock.classList.add('ContractCardComponentStyle-rarityBlock');
-			const bg = rarityBlock.style.getPropertyValue('background');
-			const match = Object.entries(RARITY_COLORS).find(([, colors]) => colors.some(color => bg.includes(color)));
-			card.setAttribute('data-rarity', match ? match[0] : '');
-		}
-
-		singleClearAllInlineStyle(card);
-		card.querySelectorAll(':scope *').forEach(el => singleClearAllInlineStyle(el));
-	}
-
+	// Must run before continuousClearAllInlineStyle handlers to read rarity from inline style first
 	onMutation(mutations => {
 		for (const { addedNodes } of mutations) {
 			for (const node of addedNodes) {
 				if (node.nodeType !== Node.ELEMENT_NODE) continue;
 				const cards = node.matches(CARD) ? [node] : [...node.querySelectorAll(CARD)];
-				cards.forEach(processCard);
+				for (const card of cards) {
+					const rarityBlock = card.querySelector('div.-backgroundImageContain + div');
+					if (!rarityBlock) continue;
+					rarityBlock.classList.add('ContractCardComponentStyle-rarityBlock');
+					const bg = rarityBlock.style.getPropertyValue('background');
+					const match = Object.entries(RARITY_COLORS).find(([, colors]) => colors.some(color => bg.includes(color)));
+					card.setAttribute('data-rarity', match ? match[0] : '');
+				}
 			}
 		}
 	});
+
+	continuousClearAllInlineStyle(CARD);
+	continuousClearAllInlineStyle(`${CARD} *`);
 })();
