798b9ac33bf169def49020e76ad0c96b5d2f44d9
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-05-31 16:15:19 +0300

    1.9.3+build130
    fix: update completed contract card styles
    
    - Change hover state styles for completed contract cards in ContractsScreen.css

--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build129
+// @version			1.9.3+build130
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
