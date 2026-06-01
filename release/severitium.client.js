5abe925080a10c0ab37dd5a9559a80fab0621dff
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-06-01 07:02:07 +0300

    1.9.3+build133
    style: update icon styles and layout adjustments
    
    - Refactor sound button icons in EntranceForms.css to use CSS variables
    - Adjust layout properties in MatchmakingWaitBlock.css for better alignment
    - Set padding and box-sizing for MatchmakingWaitComponentStyle-container

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build132';
+	const CLIENT_VERSION = '1.9.3+build133';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build132
+// @version			1.9.3+build133
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
