9cf5e34a126ad3ac7d818133e792c71569c3affc
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-06-04 17:26:52 +0300

    1.9.3+build135
    style: update navigation menu styles
    
    - Adjusted height for navigation menu container in SettingsContent.css

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build134';
+	const CLIENT_VERSION = '1.9.3+build135';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build134
+// @version			1.9.3+build135
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
