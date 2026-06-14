134d9104a1d0a87b3e55ef10b6c40575382a1230
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-06-14 13:24:32 +0300

    1.9.3+build141
    chore: minor patch
    
    - Maintenance release

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build140';
+	const CLIENT_VERSION = '1.9.3+build141';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build140
+// @version			1.9.3+build141
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
