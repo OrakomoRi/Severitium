875b48c748db2133edfcf7bb5a274db12b22f537
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-06-20 18:02:12 +0300

    1.9.3+build148
    feat: enhance modal and XP styles
    
    - Add gap to grid layout in Modal.css
    - Refine CSS selectors for experience icons in XP.css
    - Adjust settings title alignment in SettingsContent.css

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build147';
+	const CLIENT_VERSION = '1.9.3+build148';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build147
+// @version			1.9.3+build148
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
