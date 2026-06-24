5ddd4b0dc8eb1165730949b85bf4c70b005f0a79
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-06-24 13:00:00 +0300

    1.9.3+build155
    style: update button background color and opacity
    
    - Change background color to var(--severitium-gray-color) in PreviewScreen.css
    - Adjust opacity to .2 in PreviewScreen.css

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build154';
+	const CLIENT_VERSION = '1.9.3+build155';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build154
+// @version			1.9.3+build155
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
