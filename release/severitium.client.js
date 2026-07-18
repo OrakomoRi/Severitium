fed43b072de8cf116f42b4446358f05feae011d5
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-07-18 14:47:14 +0300

    1.9.3+build156
    feat: enhance two-factor authentication UI
    
    - Add introductory text styling for security settings
    - Update layout for first and second step blocks in two-factor authentication
    - Introduce divider line for two-factor authentication section

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.9.3+build155';
+	const CLIENT_VERSION = '1.9.3+build156';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.9.3+build155
+// @version			1.9.3+build156
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
