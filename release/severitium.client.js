4c5c02a18cf214826b14b2b81a80b65a3e2f0a57
OrakomoRi <60033555+OrakomoRi@users.noreply.github.com>
2026-09-19 01:22:51 +0300

    1.10.0+build4
    chore: modify cleanup process
    
    - Move cleanup of old Vercel deployments to a dedicated cleanup job
    - Ensure cleanup runs only if the build-and-release job succeeds

--- release/severitium.client.js
+++ release/severitium.client.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 
-	const CLIENT_VERSION = '1.10.0+build3';
+	const CLIENT_VERSION = '1.10.0+build4';
 
 	const isLogging = false;
 
--- release/severitium.user.js
+++ release/severitium.user.js
@@ -2,7 +2,7 @@
 
 // @name			Severitium
 // @namespace		TankiOnline
-// @version			1.10.0+build3
+// @version			1.10.0+build4
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
